78eacede598175d742b3baaf7b162e01f8de1610
William Bowling <will@wbowling.info>
2017-12-02 00:03:58 +1100

    Add ast parser

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::ascii::AsciiExt;
+use std::slice::Iter;
 
 extern crate itertools;
 use itertools::Itertools;
 
 fn main() {
     let tokens = lex("testcases/return_2.c");
-    println!("{:?}", tokens);
+    let prog = parse_program(tokens.iter());
+    println!("{:?}", prog);
 }
 
 #[derive(Debug)]
@@ -19,8 +21,100 @@ enum Token {
     SemiColon,
     Keyword(String),
     Identifier(String),
-    Literal(String),
+    Literal(u32),
     Unknown
+}
+
+#[derive(Debug)]
+struct Program {
+    func: Function
+}
+
+#[derive(Debug)]
+struct Function {
+    name: String,
+    statement: Statement
+}
+
+#[derive(Debug)]
+enum Statement {
+    Return(Expression)
+}
+
+#[derive(Debug)]
+enum Expression {
+    Int(u32)
+}
+
+fn parse_program(tokens: Iter<Token>) -> Program {
+    let (fun, mut tokens) = parse_function(tokens);
+    match tokens.next() {
+        None => Ok(()),
+        _ => Err("Should be at the end")
+    }.expect("parse error");
+
+    Program { func: fun }
+}
+
+fn parse_function(mut tokens: Iter<Token>) -> (Function, Iter<Token>) {
+    match tokens.next() {
+        Some(&Token::Keyword(ref word)) if word == "int" => Ok(true),
+        _ => Err("Expected int")
+    }.expect("failed to parse");
+
+    let name = match tokens.next() {
+        Some(&Token::Identifier(ref n)) => Ok(n),
+        _ => Err("Expected name")
+    }.expect("failed to parse");
+
+    match tokens.next() {
+        Some(&Token::OpenParen) => Ok(()),
+        _ => Err("Expected OpenParen")
+    }.expect("failed to parse");
+
+    match tokens.next() {
+        Some(&Token::CloseParen) => Ok(()),
+        _ => Err("Expected CloseParen")
+    }.expect("failed to parse");
+
+    match tokens.next() {
+        Some(&Token::OpenBrace) => Ok(()),
+        _ => Err("Expected OpenBrace")
+    }.expect("failed to parse");
+
+    let (statements, mut tokens) = parse_statement(tokens);
+
+    match tokens.next() {
+        Some(&Token::CloseBrace) => Ok(()),
+        _ => Err("Expected CloseBrace")
+    }.expect("failed to parse");
+
+    (Function { name: name.to_owned(), statement: statements }, tokens)
+}
+
+fn parse_statement(mut tokens: Iter<Token>) -> (Statement, Iter<Token>) {
+    match tokens.next() {
+        Some(&Token::Keyword(ref word)) if word == "return" => Ok(true),
+        _ => Err("Expected return")
+    }.expect("failed to parse");
+
+    let (exp, mut tokens) = parse_expression(tokens);
+    let state = Statement::Return(exp);
+
+    let res = match tokens.next() {
+        Some(&Token::SemiColon) => Ok(state),
+        _ => Err("Expected SemiColon")
+    }.expect("failed to parse");
+
+    (res, tokens)
+}
+
+fn parse_expression(mut tokens: Iter<Token>) -> (Expression, Iter<Token>) {
+    let lit = match tokens.next() {
+        Some(&Token::Literal(word)) => Ok(Expression::Int(word)),
+        _ => Err("Expected int literal")
+    };
+    (lit.expect("Failed to parse"), tokens)
 }
 
 fn lex(path: &str) -> Vec<Token> {
@@ -56,7 +150,7 @@ fn lex(path: &str) -> Vec<Token> {
                         it.next();
                     },
                     'a'...'z' | 'A'...'Z' => {
-                        let word = it.take_while_ref(|x| x.is_ascii() && x.is_alphabetic()).collect::<String>();
+                        let word = it.take_while_ref(|x| x.is_ascii() && x.is_alphanumeric()).collect::<String>();
                         match is_keyword(&word) {
                             true =>  tokens.push(Token::Keyword(word)),
                             false => tokens.push(Token::Identifier(word))
@@ -64,7 +158,8 @@ fn lex(path: &str) -> Vec<Token> {
                     },
                     '0'...'9' => {
                         let word = it.take_while_ref(|x| x.is_ascii() && x.is_digit(10)).collect::<String>();
-                        tokens.push(Token::Literal(word))
+                        let int: u32 = word.parse().expect("Not a number");
+                        tokens.push(Token::Literal(int))
                     },
                     _ => {
                         it.next();
