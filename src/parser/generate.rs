a5c87333e2b893bbbdcabf8e3148b914f5a7d853
William Bowling <will@wbowling.info>
2017-12-20 22:56:46 +1100

    Generate a list of instructions

--- src/parser/generate.rs
+++ src/parser/generate.rs
@@ -1,41 +1,91 @@
 use super::ast::*;
 
 pub fn generate(prog: Program) -> String {
-    match prog {
-        Program { func } => func.into_iter().map(|a| { gen_function(a) }).collect(),
-    }
+    let asm: Vec<String> = match prog {
+        Program { func } => func.into_iter().map(|a| { gen_function(a).join("\n") }).collect(),
+    };
+
+    asm.join("\n")
 }
 
-fn gen_function(fun: Function) -> String {
+fn gen_function(fun: Function) -> Vec<String> {
     match fun {
-        Function { name, statement } => format!(".global _{0}\n_{0}:\n{1}", name, gen_statement(statement)),
+        Function { name, statement } => {
+            vec![
+                vec![
+                    format!(".global _{0}", name),
+                    format!("_{0}:", name)
+                ],
+                gen_statement(statement)
+            ].concat()
+        }
     }
 }
 
-fn gen_statement(stat: Statement) -> String {
+fn gen_statement(stat: Statement) -> Vec<String> {
     match stat {
-        Statement::Return(exp) => format!("{}ret\n", gen_expression(exp)),
+        Statement::Return(exp) => vec![
+            gen_expression(exp),
+            s("ret")
+        ].concat(),
     }
 }
 
-fn gen_expression(exp: Expression) -> String {
+fn gen_expression(exp: Expression) -> Vec<String> {
     match exp {
-        Expression::Int(val) => format!("movl ${}, %eax\n", val),
+        Expression::Int(val) => vec![format!("movl ${}, %eax", val)],
         Expression::UnOp(op, exp) => {
             let asm = match op {
-                UnOp::Negation => "neg %eax\n",
-                UnOp::BitComp => "not %eax\n",
-                UnOp::LogicalNeg => "cmpl $0, %eax\nmovl $0, %eax\nsete %al\n",
+                UnOp::Negation => s("neg %eax"),
+                UnOp::BitComp => s("not %eax"),
+                UnOp::LogicalNeg => sa(&[
+                    "cmpl $0, %eax",
+                    "movl $0, %eax",
+                    "sete %al"
+                ]),
             };
-            format!("{}{}", gen_expression(*exp), asm)
+            vec![gen_expression(*exp), asm].concat()
         },
         Expression::BinOp(op, exp1, exp2) => {
             match op {
-                BinOp::Addition => format!("{}push %eax\n{}pop %ecx\n addl %ecx, %eax\n", gen_expression(*exp1), gen_expression(*exp2)),
-                BinOp::Subtraction => format!("{}push %eax\n{}pop %ecx\n subl %ecx, %eax\n", gen_expression(*exp2), gen_expression(*exp1)),
-                BinOp::Multiplication => format!("{}push %eax\n{}pop %ecx\n imul %ecx, %eax\n", gen_expression(*exp1), gen_expression(*exp2)),
-                BinOp::Division => format!("{}push %eax\n{}pop %ecx\n xor %edx, %edx\nidivl %ecx\n", gen_expression(*exp2), gen_expression(*exp1)),
+                BinOp::Addition => vec![
+                    gen_expression(*exp1),
+                    s("push %eax"),
+                    gen_expression(*exp2),
+                    s("pop %ecx"),
+                    s("addl %ecx, %eax"),
+                ].concat(),
+                BinOp::Subtraction => vec![
+                    gen_expression(*exp2),
+                    s("push %eax"),
+                    gen_expression(*exp1),
+                    s("pop %ecx"),
+                    s("subl %ecx, %eax"),
+                ].concat(),
+                BinOp::Multiplication => vec![
+                    gen_expression(*exp1),
+                    s("push %eax"),
+                    gen_expression(*exp2),
+                    s("pop %ecx"),
+                    s("imul %ecx, %eax"),
+                ].concat(),
+                BinOp::Division => vec![
+                    gen_expression(*exp2),
+                    s("push %eax"),
+                    gen_expression(*exp1),
+                    s("pop %ecx"),
+                    s("xor %edx, %edx"),
+                    s("idivl %ecx"),
+                ].concat()
             }
         }
     }
+}
+
+fn s(string: &str) -> Vec<String> {
+    vec![string.to_string()]
+}
+
+fn sa(string: &[&str]) -> Vec<String> {
+    string.iter().map({|s| s.to_string() }).collect()
 }
