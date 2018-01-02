a98acdbd90013828dea94d4e21b91a2c20381ee1
William Bowling <will@wbowling.info>
2018-01-02 21:57:08 +1100

    Token refactor

--- src/parser/token.rs
+++ src/parser/token.rs
@@ -1,7 +1,45 @@
 extern crate itertools;
 use self::itertools::Itertools;
 use std::ascii::AsciiExt;
+use std::iter::Peekable;
+use std::str::Chars;
 
+struct TokenParser<'a> {
+    tokens: Vec<Token>,
+    iter: Peekable<Chars<'a>>,
+}
+
+impl<'a> TokenParser<'a> {
+    pub fn new(source: &'a String) -> TokenParser<'a> {
+        TokenParser { tokens: vec![], iter: source.chars().peekable() }
+    }
+
+    fn push(&mut self, token: Token) {
+        self.iter.next();
+        self.tokens.push(token);
+    }
+
+    fn push_back(&mut self, token: Token) {
+        self.tokens.push(token);
+    }
+
+    fn next(&mut self) -> Option<char> {
+        self.iter.next()
+    }
+
+    fn drop(&mut self) {
+        self.iter.next();
+    }
+
+    fn peek(&mut self) -> Option<&char> {
+        self.iter.peek()
+    }
+
+    fn get_string<F>(&mut self, func: F) -> String
+        where F : Fn(&char) -> bool {
+        self.iter.peeking_take_while(|c| func(c)).collect()
+    }
+}
 
 #[derive(Debug)]
 pub enum Token {
@@ -27,39 +65,34 @@ pub enum Token {
     LessThan,
     LessThanOrEqual,
     GreaterThan,
-    GreaterThanOrEqual
+    GreaterThanOrEqual,
 
 }
 
 pub fn lex(contents: String) -> Vec<Token> {
-    let mut tokens: Vec<Token> = Vec::new();
+    let mut tokens = TokenParser::new(&contents);
 
-    let mut it = contents.chars().peekable();
     loop {
-        match it.next() {
-            Some(c) => {
+        match tokens.peek() {
+            Some(&c) => {
                 match c {
                     '{' => tokens.push(Token::OpenBrace),
                     '}' => tokens.push(Token::CloseBrace),
                     '(' => tokens.push(Token::OpenParen),
                     ')' => tokens.push(Token::CloseParen),
                     ';' => tokens.push(Token::SemiColon),
-                    ' ' | '\t' | '\n' | '\r' => (),
+                    ' ' | '\t' | '\n' | '\r' => tokens.drop(),
                     'a'...'z' | 'A'...'Z' => {
-                        let word = format!("{}{}",
-                                           c,
-                                           it.take_while_ref(|x| x.is_ascii() && x.is_alphanumeric()).collect::<String>());
+                        let word = tokens.get_string(|x| x.is_ascii() && x.is_alphanumeric());
                         match is_keyword(&word) {
-                            true =>  tokens.push(Token::Keyword(word)),
-                            false => tokens.push(Token::Identifier(word))
+                            true =>  tokens.push_back(Token::Keyword(word)),
+                            false => tokens.push_back(Token::Identifier(word))
                         }
                     },
                     '0'...'9' => {
-                        let word = format!("{}{}",
-                                           c,
-                                           it.take_while_ref(|x| x.is_ascii() && x.is_digit(10)).collect::<String>());
+                        let word = tokens.get_string(|x| x.is_ascii() && x.is_digit(10));
                         let int: u32 = word.parse().expect("Not a number");
-                        tokens.push(Token::Literal(int))
+                        tokens.push_back(Token::Literal(int))
                     },
                     '-' => tokens.push(Token::Negation),
                     '~' => tokens.push(Token::BitComp),
@@ -67,34 +100,18 @@ pub fn lex(contents: String) -> Vec<Token> {
                     '*' => tokens.push(Token::Multiplication),
                     '/' => tokens.push(Token::Division),
                     '%' => tokens.push(Token::Modulus),
-                    '<' | '>' | '!' => {
-                        match it.peek() {
-                            Some(&'=') => {
-                                it.next();
-                                match c {
-                                    '<' => tokens.push(Token::LessThanOrEqual),
-                                    '>' => tokens.push(Token::GreaterThanOrEqual),
-                                    '!' => tokens.push(Token::NotEqual),
-                                    _ => ()
-                                }
-                            },
-                            _ =>  match c {
-                                '<' => tokens.push(Token::LessThan),
-                                '>' => tokens.push(Token::GreaterThan),
-                                '!' => tokens.push(Token::LogicalNeg),
-                                _ => ()
-                            }
-                        }
-                    }
-                    other => {
-                        match (other, it.next()) {
-                            ('&', Some('&')) => tokens.push(Token::And),
-                            ('|', Some('|')) => tokens.push(Token::Or),
-                            ('<', Some('=')) => tokens.push(Token::LessThanOrEqual),
-                            ('>', Some('=')) => tokens.push(Token::GreaterThanOrEqual),
-                            ('=', Some('=')) => tokens.push(Token::Equal),
-                            ('!', Some('=')) => tokens.push(Token::NotEqual),
-                            _ => panic!("Unknown token {:?}", other),
+                    multi => {
+                        match (tokens.next().unwrap(), tokens.peek()) {
+                            ('&', Some(&'&')) => tokens.push(Token::And),
+                            ('|', Some(&'|')) => tokens.push(Token::Or),
+                            ('<', Some(&'=')) => tokens.push(Token::LessThanOrEqual),
+                            ('>', Some(&'=')) => tokens.push(Token::GreaterThanOrEqual),
+                            ('=', Some(&'=')) => tokens.push(Token::Equal),
+                            ('!', Some(&'=')) => tokens.push(Token::NotEqual),
+                            ('<', _) => tokens.push_back(Token::LessThan),
+                            ('>', _) => tokens.push_back(Token::GreaterThan),
+                            ('!', _) => tokens.push_back(Token::LogicalNeg),
+                            _ => panic!("Unknown token {:?}", multi),
                         }
                     }
                 };
@@ -102,7 +119,7 @@ pub fn lex(contents: String) -> Vec<Token> {
             None => break
         }
     }
-    tokens
+    tokens.tokens
 }
 
 fn is_keyword(word: &str) -> bool {
