56fb595bdd0015c8b21af25e5edf29800d7acd75
William Bowling <will@wbowling.info>
2018-01-07 23:07:20 +1100

    Refactor generate

--- src/parser/generate.rs
+++ src/parser/generate.rs
@@ -1,24 +1,55 @@
 use super::ast::*;
 use std::collections::HashMap;
 
-pub fn generate(prog: Program) -> String {
-    let asm: Vec<String> = match prog {
-        Program { func, globals } => {
-            let text: Vec<String> = func.into_iter().map(|a| gen_function(a).join("\n") ).collect();
-            let data : Vec<String> = globals.into_iter().map(|g| format!("_{0}: .word 0", g)).collect();
+struct Assembly {
+    asm: Vec<String>,
+}
 
-            vec![
-                text,
-                s(".data"),
-                data,
-            ].concat()
+impl Into<String> for Assembly {
+    fn into(self) -> String {
+        self.build()
+    }
+}
+
+impl Assembly {
+    fn new() -> Assembly {
+        Assembly { asm: vec![] }
+    }
+
+    fn add<S: Into<String>>(&mut self, string: S) {
+        self.asm.push(string.into())
+    }
+
+    fn add_all<S: Into<String>>(&mut self, strings: Vec<S>) {
+        for string in strings {
+            self.asm.push(string.into())
+        }
+    }
+
+    fn build(&self) -> String {
+        self.asm.join("\n")
+    }
+}
+
+pub fn generate(prog: Program) -> String {
+    let mut asm = Assembly::new();
+    match prog {
+        Program { func, globals } => {
+            for f in func {
+                asm.add(gen_function(f));
+            }
+            asm.add(".data");
+            for g in globals {
+                asm.add(format!("_{0}: .word 0", g));
+            }
         },
     };
 
-    asm.join("\n")
+    asm.build()
 }
 
-fn gen_function(fun: Function) -> Vec<String> {
+fn gen_function(fun: Function) -> Assembly {
+    let mut asm = Assembly::new();
     match fun {
         Function { name, arguments, statements, variables } => {
             let mut var_map: HashMap<String, i32> = HashMap::new();
@@ -36,219 +67,213 @@ fn gen_function(fun: Function) -> Vec<String> {
                 i -= 4;
             }
 
-            let asm_list: Vec<Vec<String>> = statements.into_iter().map(|statement| gen_statement(statement, &var_map)).collect();
-            vec![
-                vec![
-                    format!(".global _{0}", name),
-                    format!("_{0}:", name),
-                ],
-                s("pushl %ebp"),
-                s("movl %esp, %ebp"),
-                vec![format!("subl	${}, %esp", stack_size(&var_map))],
-                asm_list.concat(),
-            ].concat()
+            asm.add(format!(".global _{0}", name));
+            asm.add(format!("_{0}:", name));
+            asm.add("pushl %ebp");
+            asm.add("movl %esp, %ebp");
+            asm.add(format!("subl	${}, %esp", stack_size(&var_map)));
+
+            for statement in statements {
+                asm.add(gen_statement(statement, &var_map));
+            }
         }
     }
+
+    asm
 }
 
-fn gen_statement(stat: Statement, var_map: &HashMap<String, i32>) -> Vec<String> {
+fn gen_statement(stat: Statement, var_map: &HashMap<String, i32>) -> Assembly {
+    let mut asm = Assembly::new();
+
     match stat {
-        Statement::Return(exp) => vec![
-            gen_expression(exp, var_map),
-            vec![format!("addl	${}, %esp", stack_size(var_map))],
-            s("popl %ebp"),
-            s("ret\n")
-        ].concat(),
-        Statement::Declare(name, exp) | Statement::Assign(name, exp) => vec![
-            gen_expression(exp, var_map),
-            vec![format!("movl %eax, {}(%ebp)", var_map.get(&name).expect("Variable not found"))]
-        ].concat(),
+        Statement::Return(exp) => {
+            asm.add(gen_expression(exp, var_map));
+            asm.add(format!("addl	${}, %esp", stack_size(var_map)));
+            asm.add("popl %ebp");
+            asm.add("ret\n");
+        }
+        Statement::Declare(name, exp) | Statement::Assign(name, exp) => {
+            asm.add(gen_expression(exp, var_map));
+            asm.add(format!("movl %eax, {}(%ebp)", var_map.get(&name).expect("Variable not found")));
+        },
     }
+    asm
 }
 
-fn gen_expression(exp: Expression, var_map: &HashMap<String, i32>) -> Vec<String> {
+fn gen_expression(exp: Expression, var_map: &HashMap<String, i32>) -> Assembly {
+    let mut asm = Assembly::new();
     match exp {
-        Expression::Int(val) => vec![format!("movl ${}, %eax", val)],
+        Expression::Int(val) => asm.add(format!("movl ${}, %eax", val)),
         Expression::UnOp(op, exp) => {
-            let asm = match op {
-                UnOp::Negation => s("neg %eax"),
-                UnOp::BitComp => s("not %eax"),
-                UnOp::LogicalNeg => sa(&[
+            asm.add(gen_expression(*exp, var_map));
+            match op {
+                UnOp::Negation => asm.add("neg %eax"),
+                UnOp::BitComp => asm.add("not %eax"),
+                UnOp::LogicalNeg => asm.add_all(vec![
                     "cmpl $0, %eax",
                     "movl $0, %eax",
                     "sete %al"
                 ]),
-            };
-            vec![gen_expression(*exp, var_map), asm].concat()
+            }
         },
         Expression::BinOp(op, exp1, exp2) => {
             match op {
-                BinOp::Addition => vec![
-                    gen_expression(*exp1, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp2, var_map),
-                    s("pop %ecx"),
-                    s("addl %ecx, %eax"),
-                ].concat(),
-                BinOp::Subtraction => vec![
-                    gen_expression(*exp2, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp1, var_map),
-                    s("pop %ecx"),
-                    s("subl %ecx, %eax"),
-                ].concat(),
-                BinOp::Multiplication => vec![
-                    gen_expression(*exp1, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp2, var_map),
-                    s("pop %ecx"),
-                    s("imul %ecx, %eax"),
-                ].concat(),
-                BinOp::Division => vec![
-                    gen_expression(*exp2, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp1, var_map),
-                    s("pop %ecx"),
-                    s("xor %edx, %edx"),
-                    s("idivl %ecx"),
-                ].concat(),
-                BinOp::Modulus => vec![
-                    gen_expression(*exp2, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp1, var_map),
-                    s("pop %ecx"),
-                    s("xor %edx, %edx"),
-                    s("idivl %ecx"),
-                    s("movl %edx, %eax"),
-                ].concat(),
-                BinOp::Equal => vec![
-                    gen_expression(*exp1, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp2, var_map),
-                    s("pop %ecx"),
-                    s("cmpl %ecx, %eax"),
-                    s("sete %al"),
-                ].concat(),
-                BinOp::NotEqual => vec![
-                    gen_expression(*exp1, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp2, var_map),
-                    s("pop %ecx"),
-                    s("cmpl %ecx, %eax"),
-                    s("setne %al"),
-                ].concat(),
-                BinOp::LessThan => vec![
-                    gen_expression(*exp2, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp1, var_map),
-                    s("pop %ecx"),
-                    s("cmpl %ecx, %eax"),
-                    s("setl %al"),
-                ].concat(),
-                BinOp::LessThanOrEqual => vec![
-                    gen_expression(*exp2, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp1, var_map),
-                    s("pop %ecx"),
-                    s("cmpl %ecx, %eax"),
-                    s("setle %al"),
-                ].concat(),
-                BinOp::GreaterThan => vec![
-                    gen_expression(*exp2, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp1, var_map),
-                    s("pop %ecx"),
-                    s("cmpl %ecx, %eax"),
-                    s("setg %al"),
-                ].concat(),
-                BinOp::GreaterThanOrEqual => vec![
-                    gen_expression(*exp2, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp1, var_map),
-                    s("pop %ecx"),
-                    s("cmpl %ecx, %eax"),
-                    s("setge %al"),
-                ].concat(),
-                BinOp::Or => vec![
-                    gen_expression(*exp1, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp2, var_map),
-                    s("pop %ecx"),
-                    s("orl %ecx, %eax"),
-                    s("setne %al"),
-                ].concat(),
-                BinOp::And => vec![
-                    gen_expression(*exp1, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp2, var_map),
-                    s("pop %ecx"),
-                    s("cmpl $0, %ecx"),
-                    s("setne %cl"),
-                    s("cmpl $0, %eax"),
-                    s("setne %al"),
-                    s("andb %cl, %al"),
-                ].concat(),
-                BinOp::BitwiseLeft => vec![
-                    gen_expression(*exp2, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp1, var_map),
-                    s("pop %ecx"),
-                    s("shll %cl, %eax")
-                ].concat(),
-                BinOp::BitwiseRight => vec![
-                    gen_expression(*exp2, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp1, var_map),
-                    s("pop %ecx"),
-                    s("shrl %cl, %eax")
-                ].concat(),
-                BinOp::BitwiseAnd => vec![
-                    gen_expression(*exp1, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp2, var_map),
-                    s("pop %ecx"),
-                    s("andl %ecx, %eax")
-                ].concat(),
-                BinOp::BitwiseOr => vec![
-                    gen_expression(*exp1, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp2, var_map),
-                    s("pop %ecx"),
-                    s("orl %ecx, %eax"),
-                ].concat(),
-                BinOp::BitwiseXor => vec![
-                    gen_expression(*exp1, var_map),
-                    s("push %eax"),
-                    gen_expression(*exp2, var_map),
-                    s("pop %ecx"),
-                    s("xorl %ecx, %eax"),
-                ].concat(),
+                BinOp::Addition => {
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("addl %ecx, %eax");
+                },
+                BinOp::Subtraction => {
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("subl %ecx, %eax");
+                },
+                BinOp::Multiplication => {
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("imul %ecx, %eax");
+                },
+                BinOp::Division => {
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("xor %edx, %edx");
+                    asm.add("idivl %ecx");
+                }
+                BinOp::Modulus => {
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("xor %edx, %edx");
+                    asm.add("idivl %ecx");
+                    asm.add("movl %edx, %eax");
+                },
+                BinOp::Equal => {
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("cmpl %ecx, %eax");
+                    asm.add("sete %al");
+                },
+                BinOp::NotEqual => {
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("cmpl %ecx, %eax");
+                    asm.add("setne %al");
+                },
+                BinOp::LessThan => {
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("cmpl %ecx, %eax");
+                    asm.add("setl %al");
+                },
+                BinOp::LessThanOrEqual => {
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("cmpl %ecx, %eax");
+                    asm.add("setle %al");
+                },
+                BinOp::GreaterThan => {
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("cmpl %ecx, %eax");
+                    asm.add("setg %al");
+                },
+                BinOp::GreaterThanOrEqual => {
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("cmpl %ecx, %eax");
+                    asm.add("setge %al");
+                },
+                BinOp::Or => {
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("orl %ecx, %eax");
+                    asm.add("setne %al");
+                },
+                BinOp::And => {
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("cmpl $0, %ecx");
+                    asm.add("setne %cl");
+                    asm.add("cmpl $0, %eax");
+                    asm.add("setne %al");
+                    asm.add("andb %cl, %al");
+                },
+                BinOp::BitwiseLeft => {
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("shll %cl, %eax");
+                },
+                BinOp::BitwiseRight => {
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("shrl %cl, %eax");
+                },
+                BinOp::BitwiseAnd => {
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("andl %ecx, %eax");
+                },
+                BinOp::BitwiseOr => {
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("orl %ecx, %eax");
+                },
+                BinOp::BitwiseXor => {
+                    asm.add(gen_expression(*exp1, var_map));
+                    asm.add("push %eax");
+                    asm.add(gen_expression(*exp2, var_map));
+                    asm.add("pop %ecx");
+                    asm.add("xorl %ecx, %eax");
+                },
             }
         },
         Expression::Variable(name) => {
-            vec![format!("movl {}(%ebp), %eax", var_map.get(&name).expect("variable not found"))]
-        }
+            asm.add(format!("movl {}(%ebp), %eax", var_map.get(&name).expect("variable not found")))
+        },
         Expression::FunctionCall(name, arguments) => {
             let mut i = -4;
-            let mut args= arguments.into_iter().map(|exp|{
+            for exp in arguments {
                 i += 4;
-                vec![
-                    gen_expression(exp, var_map),
-                    vec![format!("movl %eax, {}(%esp)", i)]
-                ].concat()
-            }).collect::<Vec<Vec<String>>>().concat();
-            args.push(format!("call _{}", name));
-            args
+                asm.add(gen_expression(exp, var_map));
+                asm.add(format!("movl %eax, {}(%esp)", i));
+            }
+            asm.add(format!("call _{}", name));
         }
     }
-}
-
-fn s(string: &str) -> Vec<String> {
-    vec![string.to_string()]
-}
-
-fn sa(string: &[&str]) -> Vec<String> {
-    string.iter().map({|s| s.to_string() }).collect()
+    asm
 }
 
 fn stack_size(var_map: &HashMap<String, i32>) -> i32 {
